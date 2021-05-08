755d032b0e190b27c573f9cdeb042458ffff7b77
dr34mer99 <81105122+dr34mer99@users.noreply.github.com>
2021-05-08 13:50:57 +0200

    Update shapereader.py

--- Engine/shapereader.py
+++ Engine/shapereader.py
@@ -1,8 +1,9 @@
 class shapereader(shapefile):
     
     
-    def __init__(self, self.path):
+    def __init__(self, path):
         self.path = path
 
-    def reader(self, self.path):
+    def reader(self, path):
         with shapefile.reader(self.path) as shp:
+            print(shp)
