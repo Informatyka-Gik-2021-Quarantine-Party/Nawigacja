d0dda6fe94aa3c5582a1bc277865d4fde33389a8
dr34mer99 <81105122+dr34mer99@users.noreply.github.com>
2021-05-24 12:56:19 +0200

    Delete shapereader.py

--- Engine/shapereader.py
+++
@@ -1,9 +0,0 @@
-class shapereader(shapefile):
-    
-    
-    def __init__(self, path):
-        self.path = path
-
-    def reader(self, path):
-        with shapefile.reader(self.path) as shp:
-            print(shp)
